555d9f4f158996a2f1f277dde45ae48326030751
Ryubin Kim <martinok1103@naver.com>
2020-10-04 21:03:13 +0900

    Port 4000, Datestring

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
+
+const curDate = new Date();
+const dateString = curDate.toString();
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    
+    res.end(dateString);
 });
 
 server.listen(port, hostname, () => {
